03c3f9c6bf448092acae46106824f8ed5153b349
gtorlai <gtorlai@uwaterloo.ca>
2018-06-03 17:24:50 -0400

    Added code for MC average of the overlap between mps and rbm

--- utils/overlap_mps-rbm/rbm.h
+++ utils/overlap_mps-rbm/rbm.h
@@ -5,8 +5,6 @@
 #include <random>
 #include <fstream>
 
-namespace qst{
-
 // RBM class
 class Rbm{
 
@@ -67,7 +65,6 @@ public:
     inline void SetVisibleLayer(Eigen::MatrixXd v){
         v_=v;
     }
-   
     // Initialize the network parameters
     void InitRandomPars(int seed,double sigma){
         std::default_random_engine generator(seed);
@@ -230,6 +227,5 @@ public:
     }
 
  };
-}
 
 #endif
