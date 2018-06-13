ffcd8e8aaff01e291e48d86fd83ee410f24bfdd3
Ejaaz Merali <emerali@users.noreply.github.com>
2018-06-13 13:49:51 -0400

    Add training callbacks classes; Minor tweaks to RBM; Add more observable to Tomography class (and fix energy)

---
+++ rbm/callbacks.py
@@ -0,0 +1,55 @@
+import os.path
+from pathlib import Path
+
+
+class ModelSaver:
+    def __init__(self, call_every, folder_path, rbm_name, **metadata):
+        self.folder_path = folder_path
+        self.call_every = call_every
+        self.rbm_name = rbm_name
+        self.metadata = metadata
+
+        self.path = Path(folder_path, rbm_name)
+        self.path.mkdir(parents=True, exist_ok=True)
+        self.path = self.path.resolve()
+
+    def __call__(self, rbm, epoch):
+        if epoch % self.call_every == 0:
+            rbm.save(os.path.join(self.path,
+                                  "epoch{}".format(epoch)),
+                     {k: v(rbm) for k, v in self.metadata.items()})
+
+
+class Logger:
+    def __init__(self, call_every, logger_fn, msg_gen, **msg_gen_kwargs):
+        self.call_every = call_every
+        self.logger_fn = logger_fn
+        self.msg_gen = msg_gen
+        self.msg_gen_kwargs = msg_gen_kwargs
+
+    def __call__(self, rbm, epoch):
+        if epoch % self.call_every == 0:
+            self.logger_fn(self.msg_gen(rbm, epoch, **self.msg_gen_kwargs))
+
+
+class EarlyStopping:
+    def __init__(self, call_every, tolerance, patience,
+                 metric, **metric_kwargs):
+        self.call_every = call_every
+        self.tolerance = tolerance
+        self.patience = int(patience)
+        self.metric = metric
+        self.metric_kwargs = metric_kwargs
+
+        self.past_metric_values = []
+
+    def __call__(self, rbm, epoch):
+        if epoch % self.call_every == 0:
+            self.past_metric_values.append(self.metric(rbm,
+                                                       **self.metric_kwargs))
+
+            if len(self.past_metric_values) >= self.patience:
+                change_in_metric = (self.past_metric_values[-self.patience]
+                                    - self.past_metric_values[-1])
+                if change_in_metric < self.tolerance:
+                    rbm.stop_training = True
--- tomography/tomography.py
+++ tomography/tomography.py
@@ -34,6 +34,59 @@ class Tomography:
     def _to_01(samples):
         return samples.add(1.).div(2.)
 
+    @staticmethod
+    def magnetization_cb(k, num_samples):
+        def fn(rbm):
+            samples = Tomography._to_pm1(rbm.sample(k, num_samples))
+
+            return (samples
+                    .mean(1)  # compute the magnetization of each spin config
+                    .abs()
+                    .mean()   # take average of magnetization over all samples
+                    .item())  # retrieve the numerical value
+
+        return fn
+
+    def magnetization(self, k, num_samples):
+        return self.magnetization_cb(k, num_samples)(self.rbm)
+
+    @staticmethod
+    def energy_1d_tfim_cb(h, k, num_samples):
+        r"""Compute the energy per spin site for a 1D TFIM chain
+
+        $$\langle E \rangle =
+            - \frac{1}{M} \sum_\sigma \sum_i \sigma_i \sigma_{i+1}
+            - \frac{h}{M} \sum_\sigma \frac{1}{\sqrt{p(\sigma)}}
+                          \sum_i \sqrt{p(\sigma_{-i})} $$
+        """
+        def fn(rbm):
+            samples = Tomography._to_pm1(rbm.sample(k, num_samples))
+            psis = rbm.unnormalized_probability(Tomography._to_01(samples)) \
+                      .sqrt()
+
+            interaction_term = ((samples[:, :-1] * samples[:, 1:])
+                                .sum(1)     # sum over spin sites
+                                .mean()     # average over samples
+                                .item())    # retrieve numerical value
+
+            flipped_psis = torch.zeros_like(psis)
+
+            for i in range(samples.shape[-1]):  # sum over spin sites
+                Tomography._flip_spin(i, samples)  # flip the spin at site i
+                flipped_psis += rbm.unnormalized_probability(
+                                            Tomography._to_01(samples)
+                                        ).sqrt()
+                Tomography._flip_spin(i, samples)  # flip it back
+
+            transverse_field_term = (flipped_psis
+                                     .div(psis)
+                                     .mean()    # average over samples
+                                     .item())   # retrieve numerical value
+
+            return -((interaction_term + (h * transverse_field_term))
+                     / samples.shape[-1])
+        return fn
+
     def energy_1d_tfim(self, h, k, num_samples):
         r"""Compute the energy per spin site for a 1D TFIM chain
 
@@ -42,28 +95,41 @@ class Tomography:
             - \frac{h}{M} \sum_\sigma \frac{1}{\sqrt{p(\sigma)}}
                           \sum_i \sqrt{p(\sigma_{-i})} $$
         """
-        samples = self._to_pm1(self.rbm.sample(k, num_samples))
-        psis = self.rbm.unnormalized_probability(self._to_01(samples)) \
-                       .sqrt()
+        return self.energy_1d_tfim_cb(h, k, num_samples)(self.rbm)
 
-        interaction_term = ((samples[:, :-1] * samples[:, 1:])
-                            .sum(1)   # sum over spin sites
-                            .mean()   # average the results
-                            .item())  # retrieve the numerical value
+    @staticmethod
+    def specific_heat_capacity_cb(h, k, num_samples):
+        """Computes the heat capacity per spin site
+        """
 
-        flipped_psis = torch.zeros_like(psis)
+        def fn(rbm):
+            samples = Tomography._to_pm1(rbm.sample(k, num_samples))
+            psis = rbm.unnormalized_probability(Tomography._to_01(samples)) \
+                      .sqrt()
 
-        for i in range(samples.shape[-1]):  # sum over spin sites
-            self._flip_spin(i, samples)  # flip the spin at site i
-            flipped_psis += self.rbm.unnormalized_probability(
-                                        self._to_01(samples)
-                                    ).sqrt()
-            self._flip_spin(i, samples)  # flip it back
+            interaction_term = ((samples[:, :-1] * samples[:, 1:])
+                                .sum(1)     # sum over spin sites
+                                .var()      # compute variance of samples
+                                .item())    # retrieve numerical value
 
-        transverse_field_term = (flipped_psis
-                                 .div(psis)
-                                 .mean()   # average over the samples
-                                 .item())  # retrieve the numerical value
+            flipped_psis = torch.zeros_like(psis)
 
-        return -((interaction_term + (h * transverse_field_term))
-                 / samples.shape[-1])
+            for i in range(samples.shape[-1]):  # sum over spin sites
+                Tomography._flip_spin(i, samples)  # flip the spin at site i
+                flipped_psis += rbm.unnormalized_probability(
+                                            Tomography._to_01(samples)
+                                        ).sqrt()
+                Tomography._flip_spin(i, samples)  # flip it back
+
+            transverse_field_term = (flipped_psis
+                                     .div(psis)
+                                     .var()     # compute variance of samples
+                                     .item())   # retrieve numerical value
+
+            heat_cap = -(interaction_term + (h * h * transverse_field_term))
+            return (heat_cap / samples.shape[-1])
+
+        return fn
+
+    def specific_heat_capacity(self, h, k, num_samples):
+        return self.specific_heat_capacity_cb(h, k, num_samples)(self.rbm)
