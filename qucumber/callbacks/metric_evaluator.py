eb418b112baa4adb585e432a404c2d739daddf82
Ejaaz Merali <emerali@users.noreply.github.com>
2018-08-03 11:17:19 -0400

    Plotting callback (#33)
    * Split up callbacks into separate files
    
    * Add LivePlotting callback

---
+++ qucumber/callbacks/metric_evaluator.py
@@ -0,0 +1,89 @@
+# Copyright 2018 PIQuIL - All Rights Reserved
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from .callback import Callback
+
+
+class MetricEvaluator(Callback):
+    """Evaluate and hold on to the results of the given metric(s).
+
+    This Callback is called at the end of each epoch.
+
+    .. note::
+        Since Callbacks are given to :func:`fit<qucumber.rbm.BinomialRBM.fit>`
+        as a list, they will be called in a deterministic order. It is
+        therefore recommended that instances of
+        :class:`MetricEvaluator<MetricEvaluator>` be the first callbacks in
+        the list passed to :func:`fit<qucumber.rbm.BinomialRBM.fit>`,
+        as one would often use it in conjunction with other callbacks like
+        :class:`EarlyStopping<EarlyStopping>` which may depend on
+        :class:`MetricEvaluator<MetricEvaluator>` having been called.
+
+    :param period: Frequency with which the callback evaluates the given
+                   metric(s).
+    :type period: int
+    :param metrics: A dictionary of callables where the keys are the names of
+                    the metrics and the callables take the RBM being trained
+                    as their positional argument, along with some keyword
+                    arguments. The metrics are evaluated and put into a
+                    dictionary structure resembling the structure of `metrics`.
+                    If one of the callables returns a dictionary,
+                    the keys of that dictionary will be suitably modified
+                    and will be merged with the metric dictionary.
+    :type metrics: dict(str, callable)
+    :param \**metric_kwargs: Keyword arguments to be passed to `metrics`.
+    """
+    def __init__(self, period, metrics, **metric_kwargs):
+        self.period = period
+        self.metrics = metrics
+        self.metric_values = []
+        self.last = {}
+        self.metric_kwargs = metric_kwargs
+
+    def __len__(self):
+        """Return the number of timesteps that metrics have been evaluated for.
+        """
+        return len(self.metric_values)
+
+    def get_value(self, name, index=None):
+        """Retrieve the value of the desired metric from the given timestep.
+
+        :param name: The name of the metric to retrieve.
+        :type name: str
+        :param index: The index/timestep from which to retrieve the metric.
+                      Negative indices are supported. If None, will just get
+                      the most recent value.
+        :type index: int or None
+        """
+        index = index if index is not None else -1
+        return self.metric_values[index][-1][name]
+
+    def on_epoch_end(self, rbm, epoch):
+        if epoch % self.period == 0:
+            metric_vals_for_epoch = {}
+            for metric_name, metric_fn in self.metrics.items():
+                val = metric_fn(rbm, **self.metric_kwargs)
+                if isinstance(val, dict):
+                    for k, v in val.items():
+                        key = metric_name + "_" + k
+                        metric_vals_for_epoch[key] = v
+                else:
+                    metric_vals_for_epoch[metric_name] = val
+            self.last = metric_vals_for_epoch.copy()
+            self.metric_values.append((epoch, metric_vals_for_epoch))
---
+++ qucumber/callbacks/variance_based_early_stopping.py
@@ -0,0 +1,82 @@
+# Copyright 2018 PIQuIL - All Rights Reserved
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
+import numpy as np
+
+from .callback import Callback
+
+
+class VarianceBasedEarlyStopping(Callback):
+    r"""Stop training once the model stops improving. This is a variation
+    on the :class:`EarlyStopping<EarlyStopping>` class which takes the variance
+    of the metric into account.
+    The specific criterion for stopping is:
+
+    .. math:: \left\vert\frac{M_{t-p} - M_t}{\sigma_{t-p}}\right\vert < \kappa
+
+    where :math:`M_t` is the metric value at the current evaluation
+    (time :math:`t`), :math:`p` is the "patience" parameter,
+    :math:`\sigma_t` is the variance of the metric, and
+    :math:`\kappa` is the tolerance.
+
+    This Callback is called at the end of each epoch.
+
+    :param period: Frequency with which the callback checks whether training
+                   has converged (in epochs).
+    :type period: int
+    :param tolerance: The maximum (standardized) change required to consider
+                      training as having converged.
+    :type tolerance: float
+    :param patience: How many intervals to wait before claiming the training
+                     has converged.
+    :type patience: int
+    :param metric_callback: An instance of
+        :class:`MetricEvaluator<MetricEvaluator>` which computes the metric
+        that we want to check for convergence.
+    :type metric_callback: :class:`MetricEvaluator<MetricEvaluator>`
+    :param metric_name: The name of the metric stored in `metric_callback`.
+    :type metric_name: str
+    :param variance_name: The name of the variance stored in `metric_callback`.
+    :type variance_name: str
+    """
+    def __init__(self, period, tolerance, patience,
+                 metric_callback, metric_name, variance_name):
+        self.period = period
+        self.tolerance = tolerance
+        self.patience = int(patience)
+        self.metric_callback = metric_callback
+        self.value_getter = self.metric_callback.get_value
+        self.metric_name = metric_name
+        self.variance_name = variance_name
+        self.last_epoch = None
+
+    def on_epoch_end(self, rbm, epoch):
+        if epoch % self.period == 0:
+            if len(self.metric_callback) >= self.patience:
+                change_in_metric = (
+                    self.value_getter(self.metric_name, -self.patience)
+                    - self.value_getter(self.metric_name))
+
+                std_dev = np.sqrt(
+                    self.value_getter(self.variance_name, -self.patience))
+
+                if abs(change_in_metric) < (std_dev * self.tolerance):
+                    rbm.stop_training = True
+                    self.last_epoch = epoch
