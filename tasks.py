068adcd1e47be5dc3bf51c770eee0aa598e50ebf
Ejaaz Merali <emerali@users.noreply.github.com>
2018-08-07 11:00:28 -0400

    Add license to tasks.py; Remove gh-pages and index targets from docs' Makefile; Remove all python 3.5 stuff from travis conf

--- tasks.py
+++ tasks.py
@@ -1,44 +1,71 @@
+# Copyright 2018 PIQuIL - All Rights Reserved
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 
 from invoke import task
 
-import subprocess
-
 ALLOWED_BRANCHES = ["master", "develop"]
-IGNORED_TAGS = ["v0.1.2", "v0.2.0"]
 
 
-def get_tags():
-    s = subprocess.check_output(["git", "tag", "--list"],
-                                universal_newlines=True)
-    tags = [tag for tag in s.split('\n') if tag and tag not in IGNORED_TAGS]
-    return tags
-
-
-def list_important_refs():
-    return get_tags() + ALLOWED_BRANCHES
-
+# Master should rebuild *everything*
+# tags/other branches should only build themselves
 
 @task
-def build(c, ref_name):
-    with c.cd("./docs/"):
-        with c.prefix("VERSION={} && SPHINXOPTS=-aT".format(ref_name)):
-            c.run("echo $VERSION")
-            c.run("make gh-pages")
+def build_docs(c):
+    old_ref = c.run("git rev-parse HEAD").stdout.split('\n')[0].strip()
 
+    head_name = c.run("git describe --all").stdout \
+                 .split('\n')[0].strip() \
+                 .split('/')[-1]
 
-@task
-def build_all(c):
-    refs = c.run("git tag --list", hide='out').stdout.split('\n')
-    refs = [tag for tag in refs if tag and tag not in IGNORED_TAGS]
-    refs += ALLOWED_BRANCHES
+    if head_name == "master":
+        refs = c.run("git tag --list", hide='out').stdout.split('\n')
+        refs = [tag for tag in refs if tag]
+        refs += ALLOWED_BRANCHES
+    else:
+        refs = [head_name]
 
     with c.cd("./docs/"):
         c.run("mkdir -p _static _templates")
         c.run("make clean", hide='out')
 
-    for ref in refs:
-        print()
-        c.run("git checkout " + ref)
-        build(c, ref)
+        build_dirs = []
+        for ref in refs:
+            c.run("git checkout " + ref)
 
-    c.run("git checkout master")
+            b_dir = "_build/html/{}".format(ref)
+            build_dirs += b_dir
+
+            c.run("sphinx-build -b html ./ {} -aT".format(b_dir))
+
+        if head_name == "master":
+            c.run("touch _build/html/.nojekyll")
+            c.run('echo \"<meta http-equiv=\\"refresh\\" content=\\"0; '
+                  'url=./master/index.html\\" />\" > _build/html/index.html')
+
+            for ref in refs:
+                c.run('echo _build/html/{}/index.html '.format(ref)
+                      + '>> _build/html/versions.html')
+
+    c.run("git checkout gh-pages")
+
+    # TODO: clear gh-pages file tree, and pop out build contents into root
+    # then commit and push everything
+
+    c.run("git checkout {}".format(old_ref))  # revert to old_ref once done
