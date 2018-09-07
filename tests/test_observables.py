bcc7d3cc7dcd595a2a08cd10f6f4825a0e243d4c
Roger-luo <hiroger@qq.com>
2018-09-07 14:51:56 -0400

    fix flake8 check

--- tests/test_observables.py
+++ tests/test_observables.py
@@ -1,11 +1,8 @@
 import unittest
-
-import torch
 import qucumber.observables as observables
 from qucumber.nn_states import ComplexWavefunction
 
 class TestPauli(unittest.TestCase):
-
     def test_apply(self):
         test_psi = ComplexWavefunction(2, num_hidden=3)
         test_sample = test_psi.sample(100, num_samples=1000)
