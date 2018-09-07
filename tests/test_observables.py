ae52f91fabb58ca0df6e7c1d2d0f2fdf6939bd17
Roger-luo <hiroger@qq.com>
2018-09-07 14:45:12 -0400

    add test for observables

---
+++ tests/test_observables.py
@@ -0,0 +1,17 @@
+import unittest
+
+import torch
+import qucumber.observables as observables
+from qucumber.nn_states import ComplexWavefunction
+
+class TestPauli(unittest.TestCase):
+
+    def test_apply(self):
+        test_psi = ComplexWavefunction(2, num_hidden=3)
+        test_sample = test_psi.sample(100, num_samples=1000)
+        X = observables.SigmaX()
+        X.apply(test_psi, test_sample)
+
+
+if __name__ == '__main__':
+    unittest.main()
