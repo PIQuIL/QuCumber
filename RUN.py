df4fda44c939f3ef891e89da5242da5fe596499f
Dan Sehayek <dsehayek@uwaterloo.ca>
2018-10-20 13:42:51 -0400

    Set for Stage 2

--- RUN.py
+++ RUN.py
@@ -190,49 +190,59 @@ def graphDataB(filename,numQubits):
     plt.clf()
     f.close()
 
+'''
 # Test N = 5
 produceDataB(100000,1,5,5000,60,[2,4,8,16,32,64,128,256,512],10)
 graphDataB("Data/BatchSizes/Trial1/Q5/Epochs.txt",5)
+'''
 
 ######################################################################
 
-def produceData(epochs,pbs,nbs,k,numQubits,numSamples):
+#### STAGE 2: Test various learning rates on various optimizers ######
+
+def produceData(epochs,b,k,numQubits,numSamples,lr,mT,log_every):
     '''
     Writes a datafile containing lists of fidelities and runtimes for
     several epochs for various optimizers.
 
     :param epochs: Total number of epochs to train.
     :type epochs: int
-    :param pbs: Positive batch size.
-    :type pbs: int
-    :param nbs: Negative batch size.
-    :type nbs: int
+    :param b: Batch size.
+    :type b: int
     :param k: Number of contrastive divergence steps in training.
     :type k: int
     :param numQubits: Number of qubits in the quantum state.
     :type numQubits: int
     :param numSamples: Number of samples to use from sample file.
     :type numSamples: int
+    :param lr: Learning rate.
+    :type lr: float
+    :param mT: Maximum time elapsed during training.
+    :type mT: int or float
+    :param batchSizes: List of batch sizes to try.
+    :type batchSizes: listof int
+    :param log_every: Update callbacks every this number of epochs.
+    :type log_every: int
 
     :returns: None
     '''
 
     results = []
-    results.append(trainRBM(numQubits,epochs,pbs,nbs,1,k,numSamples,torch.optim.Adadelta))
-    results.append(trainRBM(numQubits,epochs,pbs,nbs,0.001,k,numSamples,torch.optim.Adam))
-    results.append(trainRBM(numQubits,epochs,pbs,nbs,0.002,k,numSamples,torch.optim.Adamax))
-    results.append(trainRBM(numQubits,epochs,pbs,nbs,0.01,k,numSamples,torch.optim.SGD))
-    results.append(trainRBM(numQubits,epochs,pbs,nbs,0.01,k,numSamples,torch.optim.SGD,momentum=0.9))
-    results.append(trainRBM(numQubits,epochs,pbs,nbs,0.01,k,numSamples,torch.optim.SGD,momentum=0.9,nesterov=True))
+    results.append(trainRBM(numQubits,epochs,b,b,lr,k,numSamples,torch.optim.Adadelta,mT,log_every))
+    results.append(trainRBM(numQubits,epochs,b,b,lr,k,numSamples,torch.optim.Adam,mT,log_every))
+    results.append(trainRBM(numQubits,epochs,b,b,lr,k,numSamples,torch.optim.Adamax,mT,log_every))
+    results.append(trainRBM(numQubits,epochs,b,b,lr,k,numSamples,torch.optim.SGD,mT,log_every))
+    results.append(trainRBM(numQubits,epochs,b,b,lr,k,numSamples,torch.optim.SGD,mT,log_every,momentum=0.9))
+    results.append(trainRBM(numQubits,epochs,b,b,lr,k,numSamples,torch.optim.SGD,mT,log_every,momentum=0.9,nesterov=True))
 
-    datafile = open("Data/Q{0}/Text/B{1}.txt".format(numQubits,pbs),"w")
-    datafile.write("Batch size is {0}\n".format(pbs))
+    datafile = open("Data/Optimizers/Q{0}/Epochs.txt".format(numQubits),"w")
+    datafile.write("Learning rate is {0}\n".format(lr))
     datafile.write("\n")
     counter = 0
     for result in results:
         datafile.write("Optimizer is " + str(listOptimizers[counter]) + "\n")
         datafile.write("Epoch & Fidelity & Runtime" + " \n")
-        for i in range(len(result["epochs"])):
+        for i in range(len(result["times"])):
             datafile.write(str(result["epochs"][i]) + " " +
                            str(round(result["fidelities"][i].item(),6)) + " " +
                            str(round(result["times"][i],6)) + "\n")
@@ -257,7 +267,7 @@ def graphData(filename,numQubits):
     line = f.readline()
     line = line.strip("\n")
     line = line.split(" ")
-    pbs = line[3]
+    lr = line[3]
     line = f.readline()
     line = f.readline()
     fidelities = []
@@ -283,9 +293,9 @@ def graphData(filename,numQubits):
     plt.xlabel("Runtime (Seconds)")
     plt.ylabel("Fidelity")
     plt.title("Learning Curve for Various Optimizers with " +
-              r"B = {0}".format(pbs))
+              r"$\alpha = {0}$".format(lr))
     plt.legend()
-    plt.savefig("Data/Q{0}/Graphs/B{1}".format(numQubits,pbs),dpi = 200)
+    plt.savefig("Data/Optimizers/Q{0}/LC".format(numQubits),dpi = 200)
     plt.clf()
     f.close()
 
@@ -298,3 +308,7 @@ for N in Nvalues:
         produceData(1000,B,B,1,N,20000)
         graphData("Data/Q{0}/Text/B{1}.txt".format(N,B),N)
 '''
+produceData(10000,10,1,5,5000,0.01,10,1)
+graphData("Data/Optimizers/Q5/Epochs.txt",5)
+
+######################################################################
