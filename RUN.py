396bb6bd7b3ad8064f6739de8df05ef1e66934f5
Dan Sehayek <dsehayek@uwaterloo.ca>
2018-10-25 23:13:11 -0400

    Can Now Specify Trial Number

--- RUN.py
+++ RUN.py
@@ -8,17 +8,17 @@ opts = [torch.optim.SGD,torch.optim.Adam]
 
 def RUN(test):
     if test == "BatchSizes":
-        HP.produceData(5,100000,batchSizes,0.01,1,20000,torch.optim.SGD,15,1,0)
+        HP.produceData(5,100000,batchSizes,0.01,1,20000,torch.optim.SGD,15,1,0,"Next")
         HP.graphData("BatchSizes",5,1,"Learning Curve")
     elif test == "GibbsSampling":
-        HP.produceData(5,100000,16,0.01,kValues,20000,torch.optim.SGD,15,1,0)
+        HP.produceData(5,100000,16,0.01,kValues,20000,torch.optim.SGD,15,1,0,"Next")
         HP.graphData("GibbsSampling",5,1,"Learning Curve")
     elif test == "LearningRates":
-        HP.produceData(5,100000,16,lrs,4,20000,torch.optim.SGD,15,1,0)
+        HP.produceData(5,100000,16,lrs,4,20000,torch.optim.SGD,15,1,0,"Next")
         HP.graphData("LearningRates",5,1,"Learning Curve")
     elif test == "Optimizers":
-        HP.produceData(5,100000,16,lrs,4,20000,opts,15,1,0)
+        HP.produceData(5,100000,16,lrs,4,20000,opts,15,1,0,"Next")
         HP.graphData("Optimizers",5,1,"Learning Curve")
     elif test == "TryThis":
-        HP.produceData(5,100000,16,0.01,1,20000,torch.optim.SGD,15,1,0)
+        HP.produceData(5,100000,16,0.01,1,20000,torch.optim.SGD,15,1,0,"Next")
         HP.graphData("TryThis",5,1,"Learning Curve")
