dc529f3a382e562d7120d9d9ca9f7c1c3c7149d7
Dan Sehayek <dsehayek@uwaterloo.ca>
2018-11-13 13:07:58 -0500

    Added Results

--- ScalingPlots.py
+++ ScalingPlots.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-numQubits = [10,15,20,25,30,40,50,60]
-nh = [5,7,10,14,16,22,28,33]
+numQubits = [10,15,20,25,30,40,50,60,70,80,90,100,125]
+nh = [5,7,10,14,16,22,28,33,39,45,50,57,71]
 
 slope, intercept = np.polyfit(numQubits,nh,1)
 lineValues = [slope * i + intercept for i in numQubits]
