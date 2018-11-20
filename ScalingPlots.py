1a78a8962aa876446fd28b34a0a61cd14a31e5ab
Dan Sehayek <dsehayek@uwaterloo.ca>
2018-11-20 09:42:07 -0500

    Editted Plots

--- ScalingPlots.py
+++ ScalingPlots.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-numQubits = [10,15,20,25,30,40,50,60,70,80,90,100,125]
-nh = [5,7,10,14,16,22,28,33,39,45,50,57,71]
+numQubits = [10,20,30,40,50,60,70,80,90,100]
+nh = [5,10,16,22,28,33,39,45,50,57]
 
 slope, intercept = np.polyfit(numQubits,nh,1)
 lineValues = [slope * i + intercept for i in numQubits]
 
 plt.plot(numQubits,nh,"o")
 plt.plot(numQubits,lineValues,"b")
-plt.title(r"$N_{h}$ Required for ROE $\leq 2 \cdot 10^{-3}$ with 99% Confidence")
+plt.title(r"$Min N_{h}$ Required for ROE $\leq 2 \cdot 10^{-3}$ with 99% Confidence")
 plt.xlabel("Number of Qubits")
 plt.ylabel("$N_{h}$")
-plt.show()
+plt.savefig("NhPlot",dpi = 200)
+plt.clf()
 
 numQubits = [10,20,30,40,50,60,70,80,90,100]
 numSamples = [7500,15000,20000,27500,35000,42500,47500,57500,65000,72500]
@@ -31,4 +32,4 @@ plt.plot(numQubits,lineValues,"b")
 plt.title(r"Min M Required for ROE $\leq 2 \cdot 10^{-3}$ with 99% Confidence")
 plt.xlabel("Number of Qubits")
 plt.ylabel("Number of Samples")
-plt.show()
+plt.savefig("MPlot",dpi = 200)
