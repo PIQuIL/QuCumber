8903ef12a306a3b5f192d056c3444a3052b74e6a
Ejaaz Merali <emerali@users.noreply.github.com>
2019-04-28 02:46:13 -0400

    Fix SWAP/Renyi observable and docs; Update tutorials

--- qucumber/observables/entanglement.py
+++ qucumber/observables/entanglement.py
@@ -17,39 +17,59 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import torch
 from .observable import ObservableBase
 from qucumber.utils import cplx
 
 
 def swap(s1, s2, A):
-    for a in A:
-        _s = s1[:, a].clone()
-        s1[:, a] = s2[:, a]
-        s2[:, a] = _s
+    r"""Applies the Swap operator onto two sets of samples, `s1` and `s2`.
 
+    The sites that will be swapped are defined by `A`.
+
+    :param s1: A rank 2 tensor of spin samples representing the first replica.
+    :type s1: torch.Tensor
+    :param s2: A rank 2 tensor of spin samples representing the second replica.
+    :type s2: torch.Tensor
+    :param A: The sites that will be swapped between the two replicas.
+    :type A: int or list or np.array or torch.Tensor
+    """
+    _s = s1[:, A].clone()
+    s1[:, A] = s2[:, A]
+    s2[:, A] = _s
     return s1, s2
 
 
-class RenyiEntropy(ObservableBase):
-    r"""The :math:`\sigma_y` observable
+class SWAP(ObservableBase):
+    r"""The :math:`\text{Swap}_A` observable.
 
-    Computes the 2nd Renyi entropy of the region A based on the SWAP operator.
+    Can be used to compute the 2nd Renyi entropy of the region A through:
+
+    :math:`S_2 = -\ln\langle \text{Swap}_A \rangle`
+
     Ref: PhysRevLett.104.157201
+
+    :param A: The sites contained in the region A.
+    :type A: int or list or np.array or torch.Tensor
     """
 
-    def __init__(self):
+    def __init__(self, A):
         self.name = "SWAP"
         self.symbol = "S"
+        self.A = A
 
-    def apply(self, nn_state, samples, A):
-        r"""Computes the entanglement entropy via a swap operator which an esimtaor for the 2nd Renyi entropy.
-        The swap operator requires an access to two identical copies of a wavefunction. In practice, this translates
-        to the requirement of having two independent sets of samples from the wavefunction replicas. For this
-        purpose, the batch of samples stored in the param samples is split into two subsets. Although this
-        procedure is designed to break the autocorrelation between the samples, it must be used with caution.
-        For a fully unbiased estimate of the entanglement entropy, the batch of samples needs to be built from two
-        independent initializations of the wavefucntion each having a different random number generator.
+    def apply(self, nn_state, samples):
+        r"""Computes the swap operator which an estimator for the 2nd Renyi
+        entropy.
+
+        The swap operator requires access to two identical copies of a
+        wavefunction. In practice, this translates to the requirement of
+        having two independent sets of samples from the wavefunction replicas.
+        For this purpose, the batch of samples stored in the param samples is
+        split into two subsets. Although this procedure is designed to break
+        the autocorrelation between the samples, it must be used with caution.
+        For a fully unbiased estimate of the entanglement entropy, the batch
+        of samples needs to be built from two independent initializations of
+        the wavefunction each having a different random number generator.
 
         :param nn_state: The WaveFunction that drew the samples.
         :type nn_state: qucumber.nn_states.WaveFunctionBase
@@ -71,11 +91,10 @@ class RenyiEntropy(ObservableBase):
         psi_ket = cplx.elementwise_mult(psi_ket1, psi_ket2)
         psi_ket_star = cplx.conjugate(psi_ket)
 
-        samples1_, samples2_ = swap(samples1, samples2, A)
+        samples1_, samples2_ = swap(samples1, samples2, self.A)
         psi_bra1 = nn_state.psi(samples1_)
         psi_bra2 = nn_state.psi(samples2_)
 
         psi_bra = cplx.elementwise_mult(psi_bra1, psi_bra2)
         psi_bra_star = cplx.conjugate(psi_bra)
-        EE = -torch.log(cplx.elementwise_division(psi_bra_star, psi_ket_star).mean(1))
-        return EE
+        return cplx.real(cplx.elementwise_division(psi_bra_star, psi_ket_star))
