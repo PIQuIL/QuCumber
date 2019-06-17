7a8e241593a7a89cde2f70aea37ac18be2d53068
DanSehayek <dsehayek@uwaterloo.ca>
2019-06-17 15:44:06 -0400

    Adjusted for Scaling

--- Energy.py
+++ Energy.py
@@ -1,6 +1,7 @@
 import os.path
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 
 from qucumber.nn_states import PositiveWavefunction
 from qucumber.callbacks import MetricEvaluator
@@ -12,6 +13,72 @@ from Ising import TFIMChainEnergy
 import qucumber.utils.training_statistics as ts
 import qucumber.utils.data as data
 
+def makeDir(path):
+    '''
+    Make directories to avoid forcing user to do so.
+
+    :param path: Folder to create
+    :type path: str
+
+    :returns: None
+    '''
+
+    try:
+        os.mkdir(path)
+    except:
+        pass
+
+def earlyStopping(ROEs,tol,pat,req):
+    '''
+    Stops training early is defined criteria is met or desired
+    improvement over certain number of epochs is not achieved.
+
+    :param resultsfile: Name of file containing results
+    :type resultsfile: str
+    :param tol: Minimum required improvement
+    :type tol: float
+    :param pat: Maximum number of epochs allowed for minimum
+                required improvement to be achieved.
+    :type pat: float
+    :param req: Goal for upper limit on ROE
+    :type req: float
+
+    :return: Status string if early stopping conditions met
+             False otherwise
+    :rtype: str or bool
+    '''
+
+    # Loop through all ROEs in case we simply want to look
+    # at a results file where early stopping was not called
+    for i in range(len(ROEs)):
+        stop = True
+        passed = False
+
+        # If requirement met then stop
+        if ROEs[i] < req:
+            passed = True
+
+        # If improvement requirement met then do not stop
+        if not passed and len(ROEs) - i > pat:
+            for j in range(1,pat + 1):
+                if ROEs[i] - ROEs[i + j] > tol:
+                    stop = False
+
+        # If epoch is less than patience tnen continue
+        elif not passed and len(ROEs) - i <= pat:
+            stop = False
+
+        if stop:
+            if passed:
+                string = "Stopped at Upper ROE = " + str(ROEs[i])
+                string += " (Passed!)"
+            else:
+                string = "Stopped at Upper ROE = " + str(ROEs[i + pat])
+                string += " (Early Stopping)"
+            return string
+
+    return False
+
 def trainEnergy(numQubits,
                 nh,
                 numSamples1 = 10000,
@@ -22,7 +89,10 @@ def trainEnergy(numQubits,
                 trial = 1,
                 storeFidelities = False,
                 plotError = False,
-                model = "Heisenberg1DFM"):
+                model = "Heisenberg1DFM",
+                earlyStoppingParams = [0.0005,50,0.0005],
+                seeds = [777,888,999],
+                study = "Nh"):
     '''
     Trains RBM on samples using energy observable as metric.
 
@@ -70,7 +140,6 @@ def trainEnergy(numQubits,
         train_data = data.load_data(train_path, numSamples=numSamples1)[0]
 
     nv = train_data.shape[-1]
-    nn_state = PositiveWavefunction(num_visible=nv, num_hidden=nh)
 
     if model == "Heisenberg1D":
         epochs = 10000
@@ -93,146 +162,166 @@ def trainEnergy(numQubits,
         modelEnergy = TFIMChainEnergy(1)
 
     if storeFidelities:
+        nn_state = PositiveWavefunction(num_visible=nv, num_hidden=nh)
         space = nn_state.generate_hilbert_space(nv)
 
-    if storeFidelities:
-        callbacks = [
-            ObservableEvaluator(
-                log_every,
-                [modelEnergy],
-                verbose=True,
-                num_samples=numSamples2,
-                burn_in=burn_in,
-                steps=steps,
-            ),
-            MetricEvaluator(
-                log_every,
-                {"Fidelity": ts.fidelity},
-                target_psi=true_psi,
-                verbose=True,
-                space=space
-            ),
-            Timer(mT,log_every,verbose = True)
-        ]
-    else:
-        callbacks = [
-            ObservableEvaluator(
-                log_every,
-                [modelEnergy],
-                verbose=True,
-                num_samples=numSamples2,
-                burn_in=burn_in,
-                steps=steps,
-            ),
-            Timer(mT,log_every,verbose = True)
-        ]
+    tol = earlyStoppingParams[0]
+    pat = earlyStoppingParams[1]
+    req = earlyStoppingParams[2]
 
-    nn_state.fit(
-        train_data,
-        epochs=epochs,
-        pos_batch_size=pbs,
-        neg_batch_size=nbs,
-        lr=lr,
-        k=k,
-        callbacks=callbacks,
-    )
+    for seed in seeds:
 
-    if model == "Heisenberg1D":
-        energies = callbacks[0].Heisenberg1DEnergy.mean
-        errors = callbacks[0].Heisenberg1DEnergy.std_error
-        variance = callbacks[0].Heisenberg1DEnergy.variance
-        median = callbacks[0].Heisenberg1DEnergy.median
-    elif model == "TFIM1D":
-        energies = callbacks[0].TFIMChainEnergy.mean
-        errors = callbacks[0].TFIMChainEnergy.std_error
-        variance = callbacks[0].TFIMChainEnergy.variance
-        median = callbacks[0].TFIMChainEnergy.median
+        torch.manual_seed(seed)
+        currentNh = nh
+        currentM = numSamples1
+        passed = False
 
-    lls = []
-    uls = []
-    mmRatio = []
-    minError = []
-    maxError = []
+        # Each iteration will correspond to an increment in Nh or M
+        while not passed:
 
-    obsFile = open("Samples/{0}/{1}Q/Observables.txt".format(model,numQubits))
-    obsFile.readline()
-    line = obsFile.readline()
-    H = round(float(line.strip("\n").split(" ")[1]),2)
+            if storeFidelities:
+                callbacks = [
+                    ObservableEvaluator(
+                        log_every,
+                        [modelEnergy],
+                        verbose=True,
+                        num_samples=numSamples2,
+                        burn_in=burn_in,
+                        steps=steps,
+                    ),
+                    MetricEvaluator(
+                        log_every,
+                        {"Fidelity": ts.fidelity},
+                        target_psi=true_psi,
+                        verbose=True,
+                        space=space
+                    ),
+                    Timer(mT,log_every,verbose = True)
+                ]
+            else:
+                callbacks = [
+                    ObservableEvaluator(
+                        log_every,
+                        [modelEnergy],
+                        verbose=True,
+                        num_samples=numSamples2,
+                        burn_in=burn_in,
+                        steps=steps,
+                    ),
+                    Timer(mT,log_every,verbose = True)
+                ]
 
-    C = 2.576
-    for i in range(len(energies)):
-        mean = energies[i]
-        med = median[i]
-        mmRatio.append(mean/med)
-        std = np.sqrt(variance[i])
-        ll = mean - C * std / np.sqrt(numSamples2)
-        ul = mean + C * std / np.sqrt(numSamples2)
-        lls.append(ll)
-        uls.append(ul)
-        lld = abs(ll - H)/abs(H)
-        uld = abs(ul - H)/abs(H)
-        if lld > uld:
-            minError.append(uld)
-            maxError.append(lld)
-        else:
-            minError.append(lld)
-            maxError.append(uld)
+            nn_state = PositiveWavefunction(num_visible=nv,num_hidden=currentNh)
+            roes = []
+            lls = []
+            uls = []
+            mmRatio = []
+            minError = []
+            maxError = []
+            fidelities = []
 
-    epoch = np.arange(log_every, len(energies) * log_every + 1, log_every)
-    epoch.astype(int)
-    nn_state.save("Data/{0}/Energy/Q{1}/Trial{2}.pt".format(model,numQubits,trial))
+            # Run over several epochs until passed or converged
+            for i in range(10000):
 
-    if plotError:
-        ax = plt.axes()
-        ax.plot(epoch, energies, color = "red")
-        ax.set_xlim(left = 1)
-        ax.axhline(H,color = "black")
-        ax.fill_between(epoch, energies - errors, energies + errors, alpha = 0.2, color = "black")
-        ax.set_xlabel("Epoch")
-        ax.set_ylabel("Energy")
-        ax.grid()
-        plt.title("Samples = {0}".format(numSamples2) +
-                  " & Burn In = {0}".format(burn_in) +
-                  " & Steps = {0} for N = {1}".format(steps,numQubits))
-        plt.tight_layout()
-        plt.savefig("Data/{0}/Energy/Q{1}/Trial{2}".format(model,numQubits,trial))
+                nn_state.fit(
+                    train_data,
+                    epochs=log_every,
+                    pos_batch_size=pbs,
+                    neg_batch_size=nbs,
+                    lr=lr,
+                    k=k,
+                    callbacks=callbacks,
+                )
 
-    if storeFidelities:
-        fidelities = callbacks[1].Fidelity
-        runtimes = callbacks[2].epochTimes
-    else:
-        runtimes = callbacks[1].epochTimes
-    relativeErrors = []
-    stdErrors = []
-    for i in range(len(energies)):
-        relativeErrors.append(abs(energies[i] - H)/abs(H))
-        stdErrors.append(C * np.sqrt(variance[i])/np.sqrt(numSamples2))
+                if storeFidelities:
+                    fidelity = callbacks[1].Fidelity
+                    fidelities.append(fidelity[0])
 
-    resultsfile = open("Data/{0}/Energy/Q{1}/Trial{2}.txt".format(model,numQubits,trial),"w")
-    resultsfile.write("nh: " + str(nh) + "\n")
-    resultsfile.write("samples1: " + str(numSamples1) + "\n")
-    resultsfile.write("samples2: " + str(numSamples2) + "\n")
-    resultsfile.write("burn_in: " + str(burn_in) + "\n")
-    resultsfile.write("steps: " + str(steps) + "\n")
-    resultsfile.write("Exact H:" + str(H) + "\n")
-    resultsfile.write("   Fidelity  ROE       Mean      Median    CI Width LL on CI  UL on CI  mmRatio  Min Err  Max Err\n")
-    for i in range(len(energies)):
-        resultsfile.write(str(epoch[i]) + "  ")
-        if storeFidelities:
-            resultsfile.write("%.6f" % round(float(fidelities[i]),6) + "  ")
-        else:
-            resultsfile.write("   N/A  " + "  ")
-        resultsfile.write("%.6f" % round(float(relativeErrors[i]),6) + "  ")
-        resultsfile.write("%.5f" % round(float(energies[i]),5) + "  ")
-        resultsfile.write("%.5f" % round(float(median[i]),5) + "  ")
-        resultsfile.write("%.5f" % round(float(stdErrors[i]),5) + "  ")
-        resultsfile.write("%.5f" % round(float(lls[i]),5) + "  ")
-        resultsfile.write("%.5f" % round(float(uls[i]),5) + "  ")
-        resultsfile.write("%.5f" % round(float(mmRatio[i]),5) + "  ")
-        resultsfile.write("%.5f" % round(float(minError[i]),5) + "  ")
-        resultsfile.write("%.5f" % round(float(maxError[i]),5) + "  \n")
+                if model == "Heisenberg1D":
+                    energies = callbacks[0].Heisenberg1DEnergy.mean
+                    errors = callbacks[0].Heisenberg1DEnergy.std_error
+                    variance = callbacks[0].Heisenberg1DEnergy.variance
+                    median = callbacks[0].Heisenberg1DEnergy.median
+                elif model == "TFIM1D":
+                    energies = callbacks[0].TFIMChainEnergy.mean
+                    errors = callbacks[0].TFIMChainEnergy.std_error
+                    variance = callbacks[0].TFIMChainEnergy.variance
+                    median = callbacks[0].TFIMChainEnergy.median
 
-    resultsfile.close()
+                obsFile = open("Samples/{0}/{1}Q/Observables.txt".format(model,numQubits))
+                obsFile.readline()
+                line = obsFile.readline()
+                H = round(float(line.strip("\n").split(" ")[1]),2)
+
+                C = 2.576
+                mean = energies[i]
+                med = median[i]
+                mmRatio.append(mean/med)
+                std = np.sqrt(variance[i])
+                roe = abs(mean - H)/abs(H)
+                roes.append(roe)
+                ll = mean - C * std / np.sqrt(numSamples2)
+                ul = mean + C * std / np.sqrt(numSamples2)
+                lls.append(ll)
+                uls.append(ul)
+                lld = abs(ll - H)/abs(H)
+                uld = abs(ul - H)/abs(H)
+                if lld > uld:
+                    minError.append(uld)
+                    maxError.append(lld)
+                else:
+                    minError.append(lld)
+                    maxError.append(uld)
+
+                esStatus = earlyStopping(maxError,tol,pat,req)
+                if esStatus != False:
+                    print("#" * 52)
+                    print(esStatus)
+                    print("#" * 52)
+                    if esStatus[-2] == "!":
+                        passed = True
+                    else:
+                        if study == "Nh":
+                            currentNh += 1
+                        elif study == "M":
+                            currentM += 1000
+                    break
+
+            path1 = "Data/{0}Study".format(study)
+            path2 = path1 + "/Q{0}".format(numQubits)
+            path3 = path2 + "/{0}".format(seed)
+            if study == "Nh":
+                path4 = path3 + "/{0}{1}".format(study,currentNh)
+            else:
+                path4 = path3 + "/{0}{1}".format(study,currentM)
+
+            makeDir(path1)
+            makeDir(path2)
+            makeDir(path3)
+            makeDir(path4)
+
+            nn_state.save(path4 + "/model.pt")
+            epochs = np.arange(log_every, len(energies) * log_every + 1, log_every)
+            epochs.astype(int)
+
+            numParams = numQubits * currentNh + numQubits + currentNh
+            rfile = open(path4 + "/Results.txt","w")
+            rfile.write("Number of Qubits: " + str(numQubits) + "\n")
+            rfile.write("Number of Samples: " + str(currentM) + "\n")
+            rfile.write("Hidden Units: " + str(currentNh) + "\n")
+            rfile.write("Number of Parameters: " + str(numParams) + "\n")
+            rfile.write("Energy Samples: " + str(numSamples2) + "\n")
+            rfile.write("Burn In: " + str(burn_in) + "\n")
+            rfile.write("Steps: " + str(steps) + "\n")
+
+            for i in range(len(epochs)):
+                rfile.write("Epoch {0}:   ".format(epochs[i]))
+                rfile.write("ROE: {0:.8f}   ".format(roes[i]))
+                rfile.write("ROE Upper Bound: {0:.8f}   ".format(maxError[i]))
+                rfile.write("MM Ratio: {0:.4f}   ".format(mmRatio[i]))
+                rfile.write("\n")
+
+            rfile.close()
 
 def confidence(numQubits,numSamples,burn_in = 500,steps = 100,trial = 1,model = "Heisenberg1DFM"):
     '''
@@ -300,5 +389,5 @@ def confidence(numQubits,numSamples,burn_in = 500,steps = 100,trial = 1,model = 
     print("Min Error for 99% CI: {0}".format(minError))
     print("Max Error for 99% CI: {0}".format(maxError))
 
-trainEnergy(10,1,numSamples1 = 10000,numSamples2 = 5000,burn_in = 500,steps = 100,mT = 30,trial = 1,storeFidelities = True,model = "TFIM1D")
-confidence(10,10000,1000,100,trial = 1,model = "TFIM1D")
+trainEnergy(10,10,numSamples1 = 10000,numSamples2 = 5000,burn_in = 500,steps = 100,mT = 30,trial = 1,storeFidelities = True,model = "TFIM1D")
+# confidence(10,10000,1000,100,trial = 1,model = "TFIM1D")
