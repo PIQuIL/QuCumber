c9d6f73150811673bb333bb9c3245a81dccc8365
Ejaaz Merali <emerali@users.noreply.github.com>
2019-11-23 01:14:24 -0500

    setup tox travis

--- tasks.py
+++ tasks.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import pathlib
 from pprint import pformat
 from itertools import chain
